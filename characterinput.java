3d868b0ecea81b84743ea7fa01a38a1b2e61a040
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:45:14 +0530

    Add files via upload

---
+++ characterinput.java
@@ -0,0 +1,16 @@
+package Jayasrijavapractice;
+
+import java.util.Scanner;
+
+public class characterinput {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		char letter=sc.next().charAt(0);
+		System.out.println(letter);
+
+	}
+
+}
